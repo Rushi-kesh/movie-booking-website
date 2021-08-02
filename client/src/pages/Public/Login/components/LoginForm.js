c093de3919a7111a0f5250c3354d70602414b98a
Rushi-Kesh <gaikwadrushikesh88888@gmail.com>
2021-08-02 11:08:16 +0530

    make it better

--- client/src/pages/Public/Login/components/LoginForm.js
+++ client/src/pages/Public/Login/components/LoginForm.js
@@ -9,7 +9,7 @@ import { history } from '../../../../utils';
 const useStyles = makeStyles(theme => ({
   form: {
     margin:"50px 0px",
-    paddingLeft:"300px",
+    paddingLeft:"500px",
     textAlign:"center",
     align:"center",
   },
