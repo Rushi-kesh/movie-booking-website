fb7f0c6a95b721af0e1e449e614a698fb965a792
Rushi-Kesh <gaikwadrushikesh88888@gmail.com>
2021-08-05 11:27:40 +0530

    make it better

--- src/routes/users.js
+++ src/routes/users.js
@@ -44,30 +44,6 @@ router.post('/users/logout', auth.simple, async (req, res) => {
   }
 });
 
-// Logout all
-router.post('/users/logoutAll', auth.enhance, async (req, res) => {
-  try {
-    req.user.tokens = [];
-    await req.user.save();
-    res.send();
-  } catch (e) {
-    res.status(400).send(e);
-  }
-});
-
-// Get all users
-router.get('/users', auth.enhance, async (req, res) => {
-  if (req.user.role !== 'superadmin')
-    return res.status(400).send({
-      error: 'Only the god can see all the users!',
-    });
-  try {
-    const users = await User.find({});
-    res.send(users);
-  } catch (e) {
-    res.status(400).send(e);
-  }
-});
 
 // User infos
 router.get('/users/me', auth.simple, async (req, res) => {
@@ -78,94 +54,5 @@ router.get('/users/me', auth.simple, async (req, res) => {
   }
 });
 
-// Get user by id only for admin
-router.get('/users/:id', auth.enhance, async (req, res) => {
-  if (req.user.role !== 'superadmin')
-    return res.status(400).send({
-      error: 'Only the god can see the user!',
-    });
-  const _id = req.params.id;
-  try {
-    const user = await User.findById(_id);
-    if (!user) return res.sendStatus(404);
-    res.send(user);
-  } catch (e) {
-    res.sendStatus(400);
-  }
-});
-
-// Edit/Update user
-router.patch('/users/me', auth.simple, async (req, res) => {
-  const updates = Object.keys(req.body);
-  const allowedUpdates = ['name', 'username', 'email', 'password'];
-  const isValidOperation = updates.every((update) => allowedUpdates.includes(update));
-  if (!isValidOperation) return res.status(400).send({ error: 'Invalid updates!' });
-
-  try {
-    const { user } = req;
-    updates.forEach((update) => (user[update] = req.body[update]));
-    await user.save();
-    res.send(user);
-  } catch (e) {
-    res.status(400).send(e);
-  }
-});
-
-// Admin can update user by id
-router.patch('/users/:id', auth.enhance, async (req, res) => {
-  if (req.user.role !== 'superadmin')
-    return res.status(400).send({
-      error: 'Only the god can update the user!',
-    });
-  const _id = req.params.id;
-
-  const updates = Object.keys(req.body);
-  const allowedUpdates = ['name', 'username', 'email', 'password'];
-  const isValidOperation = updates.every((update) => allowedUpdates.includes(update));
-
-  if (!isValidOperation) return res.status(400).send({ error: 'Invalid updates!' });
-
-  try {
-    const user = await User.findById(_id);
-    updates.forEach((update) => (user[update] = req.body[update]));
-    await user.save();
-
-    if (!user) return res.sendStatus(404);
-    res.send(user);
-  } catch (e) {
-    res.status(400).send(e);
-  }
-});
-
-// Delete by id
-router.delete('/users/:id', auth.enhance, async (req, res) => {
-  if (req.user.role !== 'superadmin')
-    return res.status(400).send({
-      error: 'Only the god can delete the user!',
-    });
-  const _id = req.params.id;
-
-  try {
-    const user = await User.findByIdAndDelete(_id);
-    if (!user) return res.sendStatus(404);
-
-    res.send({ message: 'User Deleted' });
-  } catch (e) {
-    res.sendStatus(400);
-  }
-});
-
-router.delete('/users/me', auth.simple, async (req, res) => {
-  if (req.user.role !== 'superadmin')
-    return res.status(400).send({
-      error: 'You cannot delete yourself!',
-    });
-  try {
-    await req.user.remove();
-    res.send(req.user);
-  } catch (e) {
-    res.sendStatus(400);
-  }
-});
 
 module.exports = router;
